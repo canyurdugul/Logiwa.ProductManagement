337593012d28a49f5c7fab2a45f18254bc4ad6f7
Ömürcan Yurdugül <omurcanumutyurdugul@gmail.com>
2022-01-15 17:27:16 +0300

    Repositories created for entities.

---
+++ Logiwa.ProductManagement/03 - Database/Logiwa.ProductManagement.Database/Repositories/GenericRepository/Abstract/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using Logiwa.ProductManagement.Database.UnitOfWork.Abstracts;
+using Logiwa.ProductManagement.Entities.Base;
+using System;
+using System.Collections.Generic; 
+using System.Linq.Expressions; 
+using System.Threading.Tasks;
+
+namespace Logiwa.ProductManagement.Database.Repositories.GenericRepository.Abstract
+{
+    public interface IGenericRepository<TEntity, TKey> where TEntity : EntityBase<TKey>
+    {
+        Task<TEntity> GetByIdAsync(IUnitOfWork unitOfWork, TKey Id);
+        Task<TEntity> GetAsync(IUnitOfWork unitOfWork, Expression<Func<TEntity, bool>> predicate);
+        Task<IEnumerable<TEntity>> GetListAsync(IUnitOfWork unitOfWork);
+        Task InsertAsync(IUnitOfWork unitOfWork, TEntity entity);
+        Task UpdateAsync(IUnitOfWork unitOfWork, TEntity entity);
+        Task DeleteAsync(IUnitOfWork unitOfWork, TEntity entity);
+        Task SoftDeleteAsync(IUnitOfWork unitOfWork, TEntity entity);
+    }
+}
---
+++ Logiwa.ProductManagement/03 - Database/Logiwa.ProductManagement.Database/Repositories/ProductRepository/ProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using Logiwa.ProductManagement.Database.Repositories.GenericRepository.Concrete;
+using Logiwa.ProductManagement.Database.UnitOfWork.Abstracts;
+using Logiwa.ProductManagement.Entities.Product;
+
+namespace Logiwa.ProductManagement.Database.Repositories.ProductRepository
+{
+    public class CategoryRepository : GenericRepository<Product, int>, ICategoryRepository
+    {
+        private readonly IUnitOfWorkFactory unitOfWorkFactory;
+
+        public CategoryRepository(IUnitOfWorkFactory unitOfWorkFactory) : base(unitOfWorkFactory)
+        {
+            this.unitOfWorkFactory = unitOfWorkFactory;
+        }
+    }
+}
